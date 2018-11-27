db5a1888ec9eea56e094b006c3ef8e1aacb14181
jmjxperia <41988303+jmjxperia@users.noreply.github.com>
2018-11-27 11:45:46 +0530

    clone to legacy folder locally

--- Android.mk
+++ Android.mk
@@ -1,15 +1,6 @@
-ifneq ($(AUDIO_USE_STUB_HAL), true)
-
 MY_LOCAL_PATH := $(call my-dir)
 
-ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
-include $(MY_LOCAL_PATH)/legacy/Android.mk
-else
-ifneq ($(filter mpq8092,$(TARGET_BOARD_PLATFORM)),)
-include $(MY_LOCAL_PATH)/hal_mpq/Android.mk
-else
 include $(MY_LOCAL_PATH)/hal/Android.mk
-endif
 include $(MY_LOCAL_PATH)/hal/audio_extn/Android.mk
 include $(MY_LOCAL_PATH)/voice_processing/Android.mk
 include $(MY_LOCAL_PATH)/mm-audio/Android.mk
@@ -17,11 +8,7 @@ include $(MY_LOCAL_PATH)/visualizer/Android.mk
 include $(MY_LOCAL_PATH)/post_proc/Android.mk
 include $(MY_LOCAL_PATH)/qahw/Android.mk
 include $(MY_LOCAL_PATH)/qahw_api/Android.mk
-endif
 
 ifeq ($(USE_LEGACY_AUDIO_DAEMON), true)
 include $(MY_LOCAL_PATH)/audiod/Android.mk
-endif
-
-endif
 endif
